9c87b35cf80b994a520622214dc0396459541cfd
abdallahmoustafa94 <dimo.am@gmail.com>
2023-04-17 12:50:43 +0200

    added social icons

--- src/components/Contact-form/contact-form.jsx
+++ src/components/Contact-form/contact-form.jsx
@@ -19,15 +19,15 @@ const ContactForm = () => {
       <div className="container">
         <div className="row">
           <div className="col-lg-6">
-          <div className="map" id="ieatmaps">
-          <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13810.673262364526!2d31.3398875!3d30.0750406!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583fbb0347a73b%3A0xb23ab6a228633253!2sGlobal%20dental%20clinic!5e0!3m2!1sen!2seg!4v1681711231950!5m2!1sen!2seg"  loading="lazy" ></iframe>
-        </div>
+            <div className="map" id="ieatmaps">
+              <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d13810.673262364526!2d31.3398875!3d30.0750406!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x14583fbb0347a73b%3A0xb23ab6a228633253!2sGlobal%20dental%20clinic!5e0!3m2!1sen!2seg!4v1681711231950!5m2!1sen!2seg" loading="lazy" ></iframe>
+            </div>
           </div>
           <div className="col-lg-5 offset-lg-1">
             <div className="cont-info">
               <h4 className="fw-700  mb-50">Contact Info.</h4>
               <h3 className="wow" data-splitting>
-              Let's Talk.
+                Let's Talk.
               </h3>
               <div className="item mb-40">
                 <h5>
@@ -40,24 +40,11 @@ const ContactForm = () => {
               </h3>
               <div className="item">
                 <h6>
-                9 Omarat Al Saudeya, Masaken Al Mohandesin, Nasr City, Cairo Governorate 4451701
-                 
+                  9 Omarat Al Saudeya, Masaken Al Mohandesin, Nasr City, Cairo Governorate 4451701
+
                 </h6>
               </div>
-              <div className="social mt-50">
-                <a href="#0" className="icon">
-                  <i className="fab fa-facebook-f"></i>
-                </a>
-                <a href="#0" className="icon">
-                  <i className="fab fa-twitter"></i>
-                </a>
-                <a href="#0" className="icon">
-                  <i className="fab fa-pinterest"></i>
-                </a>
-                <a href="#0" className="icon">
-                  <i className="fab fa-behance"></i>
-                </a>
-              </div>
+
             </div>
           </div>
         </div>
