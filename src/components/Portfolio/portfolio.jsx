cf6564bf2339b90655bca9635c2f16c93f19fc00
abdallahmoustafa94 <dimo.am@gmail.com>
2023-04-18 09:51:51 +0200

    fix portfolio image link

--- src/components/Portfolio/portfolio.jsx
+++ src/components/Portfolio/portfolio.jsx
@@ -21,20 +21,18 @@ const Portfolio = ({ grid, filterPosition }) => {
           <div className="gallery full-width">
             <div
               className={`${grid === 3
-                  ? "col-lg-4 col-md-6"
-                  : grid === 2
-                    ? "col-md-6 lg-mr"
-                    : "col-12"
+                ? "col-lg-4 col-md-6"
+                : grid === 2
+                  ? "col-md-6 lg-mr"
+                  : "col-12"
                 } items graphic wow fadeInUp`}
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
-                  <a className="imago wow">
-                    <img src="/final/case1.jpeg" alt="image" />
-                    <div className="item-img-overlay"></div>
-                  </a>
-                </Link>
+                <a className="imago wow">
+                  <img src="/final/case1.jpeg" alt="image" />
+                  <div className="item-img-overlay"></div>
+                </a>
               </div>
               <div className="cont">
                 <h6 className="text-left">A Complete Zirconium Makeover for Upper and Lower Arches!</h6>
@@ -51,20 +49,18 @@ const Portfolio = ({ grid, filterPosition }) => {
 
             <div
               className={`${grid === 3
-                  ? "col-lg-4 col-md-6"
-                  : grid === 2
-                    ? "col-md-6"
-                    : "col-12"
+                ? "col-lg-4 col-md-6"
+                : grid === 2
+                  ? "col-md-6"
+                  : "col-12"
                 } items web wow fadeInUp`}
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
-                  <a className="imago wow">
-                    <img src="/final/case2.jpeg" alt="image" />
-                    <div className="item-img-overlay"></div>
-                  </a>
-                </Link>
+                <a className="imago wow">
+                  <img src="/final/case2.jpeg" alt="image" />
+                  <div className="item-img-overlay"></div>
+                </a>
               </div>
               <div className="cont">
                 <h6 className="text-left">Stunning Zirconium Crowns and Bridge Makeover!</h6>
@@ -80,20 +76,18 @@ const Portfolio = ({ grid, filterPosition }) => {
             </div>
             <div
               className={`${grid === 3
-                  ? "col-lg-4 col-md-6"
-                  : grid === 2
-                    ? "col-md-6"
-                    : "col-12"
+                ? "col-lg-4 col-md-6"
+                : grid === 2
+                  ? "col-md-6"
+                  : "col-12"
                 } items web wow fadeInUp`}
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
-                  <a className="imago wow">
-                    <img src="/final/case3.jpeg" alt="image" />
-                    <div className="item-img-overlay"></div>
-                  </a>
-                </Link>
+                <a className="imago wow">
+                  <img src="/final/case3.jpeg" alt="image" />
+                  <div className="item-img-overlay"></div>
+                </a>
               </div>
               <div className="cont">
                 <h6 className="text-left">Smile makeover done using 8 Emax porcelain veneers</h6>
@@ -109,20 +103,18 @@ const Portfolio = ({ grid, filterPosition }) => {
             </div>
             <div
               className={`${grid === 3
-                  ? "col-lg-4 col-md-6"
-                  : grid === 2
-                    ? "col-md-6"
-                    : "col-12"
+                ? "col-lg-4 col-md-6"
+                : grid === 2
+                  ? "col-md-6"
+                  : "col-12"
                 } items web wow fadeInUp`}
               data-wow-delay=".4s"
             >
               <div className="item-img">
-                <Link href={`/project-details2/project-details2-dark`}>
-                  <a className="imago wow">
-                    <img src="/final/case4.jpeg" alt="image" />
-                    <div className="item-img-overlay"></div>
-                  </a>
-                </Link>
+                <a className="imago wow">
+                  <img src="/final/case4.jpeg" alt="image" />
+                  <div className="item-img-overlay"></div>
+                </a>
               </div>
               <div className="cont">
                 <h6 className="text-left">Restoring Fallen Crowns with Emax Replacements to Match Natural Teeth</h6>
