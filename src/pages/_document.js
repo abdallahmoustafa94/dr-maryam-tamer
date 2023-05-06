9e3ed3eeada74c809936d121d53ece2c5352041c
Abdallah Moustafa <45407750+abdallahmoustafa94@users.noreply.github.com>
2023-05-06 21:26:43 +0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,4 +1,3 @@
-import { Script } from 'next/script';
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
@@ -34,23 +33,28 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
-        <Script
-        src="https://www.googletagmanager.com/gtag/js?id=GTM-THZXR2W"
-        strategy="afterInteractive"
-      />
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-THZXR2W');`,
+            }}
+          />
         </Head>
 
         <body>
           <Main />
           <NextScript />
-<noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-THZXR2W"
-          height="0"
-          width="0"
-          style="display:none;visibility:hidden"
-        ></iframe>
-      </noscript>
+ <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=GTM-THZXR2W`}
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
         </body>
       </Html>
     );
