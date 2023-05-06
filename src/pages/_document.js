0a852739857216d293cdf460aac8ed15bbe9431c
Abdallah Moustafa <45407750+abdallahmoustafa94@users.noreply.github.com>
2023-05-06 21:15:45 +0300

    Update _document.js

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -33,11 +33,18 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat+Alternates:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-THZXR2W');</script>
         </Head>
 
         <body>
           <Main />
           <NextScript />
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-THZXR2W"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
         </body>
       </Html>
     );
