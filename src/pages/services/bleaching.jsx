d739664d645149f63f3942610569c09b4bc9810b
abdallahmoustafa94 <dimo.am@gmail.com>
2023-06-17 00:47:29 +0300

    update bleaching image path

--- src/pages/services/bleaching.jsx
+++ src/pages/services/bleaching.jsx
@@ -38,7 +38,7 @@ const About = () => {
           <div className="row">
           <div className="col-lg-4">
                   <div className="wow imago valign " data-wow-delay=".3s">
-                    <img src="/final/bleaching.JPG" alt="" />
+                    <img src="/final/Bleaching.JPG" alt="" />
                   </div>
           </div>
 
